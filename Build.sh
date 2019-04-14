fc9d7991bd9afcdd5c97a89bd2c436362dccfd1a
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-04-14 16:36:26 -0400

    Look I made `cat`

--- Build.sh
+++ Build.sh
@@ -2,5 +2,5 @@ scons #build
 
 if [ $? -eq 0 ]; then #if the build succeeded
 	printf "\n\n===============================\n\n\n"
-    ./build/compiler #run the executable
+    ./build/compiler ./examples/example.mountain #run the executable
 fi
--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,10 +1,34 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 
-int main()
+int main(int arg_count, char *arg_array[])
 {
-	printf("Hello\n");
+	if(arg_count != 2)
+	{
+		printf("Please provide a file path to compile\n");
+		return EXIT_FAILURE;
+	}
+
+	FILE *source_file = fopen(arg_array[1], "r");
+	if(source_file == NULL)
+	{
+		printf("Could not open file '%s' to compile\n", arg_array[1]);
+		return EXIT_FAILURE;
+	}
+
+	char car;
+	while(true)
+	{
+		car = fgetc(source_file);
+		if(car == EOF)
+			break;
+
+		printf("%c", car);
+	}
+
+	fclose(source_file);
 	return EXIT_SUCCESS;
 }
 
