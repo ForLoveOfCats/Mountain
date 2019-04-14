080f66ea4073516b0ab235a7759ddbe5c5d872a5
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-04-14 18:04:06 -0400

    Begin token splitting

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,6 +1,48 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdio.h>
+
+
+char* next_token(FILE *source_file)
+{
+	bool in_token = false;
+	char *token = malloc(sizeof(char));
+	char car;
+	while(true)
+	{
+		car = fgetc(source_file);
+		if(car == EOF)
+		{
+			goto return_token;
+		}
+
+		if(car == ' ' || car == '\t' || car == '\n')
+		{
+			if(!in_token)
+				continue;
+			else
+				goto return_token;
+		}
+		else
+			in_token = true;
+
+		int old_token_length = strlen(token);
+		char *new_token = malloc(sizeof(char) * (old_token_length + 1));
+		strncpy(new_token, token, old_token_length);
+		free(token);
+		token = new_token;
+		token[old_token_length] = car;
+		token[old_token_length + 1] = '\0';
+	}
+
+return_token:
+	if(strlen(token) <= 1 && car == EOF) //If token is blank and is at the end of the file
+	{
+		return NULL; //Return NULL
+	}
+	return token; //Otherwise return the token
+}
 
 
 int main(int arg_count, char *arg_array[])
@@ -18,14 +60,13 @@ int main(int arg_count, char *arg_array[])
 		return EXIT_FAILURE;
 	}
 
-	char car;
 	while(true)
 	{
-		car = fgetc(source_file);
-		if(car == EOF)
+		char *token = next_token(source_file);
+		if(token == NULL)
 			break;
 
-		printf("%c", car);
+		printf("%s\n", token);
 	}
 
 	fclose(source_file);
