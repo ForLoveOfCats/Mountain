428a13264641fb10ccde9121693dfec4cea0ce87
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-04-17 08:13:19 -0400

    Move parsing into own source and header

---
+++ compiler/parser.c
@@ -0,0 +1,145 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "parser.h"
+#include "ast.h"
+
+
+struct TOKEN next_token(FILE *source_file)
+{
+	struct TOKEN token = {true, TOKEN_WORD, strdup("")};
+	bool in_token = false;
+	char car;
+	while(true)
+	{
+		car = fgetc(source_file);
+		if(car == EOF)
+		{
+			goto return_token;
+		}
+
+		if(!in_token)
+		{
+			switch(car)
+			{
+				case ':':
+				{
+					token.type = TOKEN_COLON;
+					goto return_car_as_token;
+				}
+
+				case ';':
+				{
+					token.type = TOKEN_SEMICOLON;
+					goto return_car_as_token;
+				}
+
+				case '#':
+				{
+					token.type = TOKEN_COMMENT;
+					goto return_car_as_token;
+				}
+			}
+		}
+
+		switch(car)
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ':':
+			case ';':
+			case '#':
+			{
+				if(!in_token)
+					continue;
+				else
+				{
+					ungetc(car, source_file); //HACK FIXME
+					goto return_token;
+				}
+
+				break;
+			}
+			default:
+				in_token = true;
+		}
+
+		int old_token_length = strlen(token.string);
+		char *new_token = malloc(sizeof(char) * (old_token_length+2)); //Two larger than originally
+		strcpy(new_token, token.string);
+		free(token.string);
+		token.string = new_token;
+		token.string[old_token_length] = car; //Place in first new spot
+		token.string[old_token_length + 1] = '\0'; //Null terminate in second new spot
+	}
+
+return_token: ;
+	if(strlen(token.string) <= 0 && car == EOF) //If token is blank and is at the end of the file
+	{
+		token.valid = false;
+		return token; //Return NULL
+	}
+	return token; //Otherwise return the token
+
+return_car_as_token: ;
+	free(token.string);
+	token.string = malloc(sizeof(char) * 2);
+	token.string[0] = car;
+	token.string[1] = '\0';
+	return token;
+}
+
+
+bool parse_next_statement(FILE *source_file)
+{
+	struct TOKEN token = next_token(source_file);
+	if(!token.valid)
+	{
+		printf("Reached end of file\n");
+		free(token.string);
+		return false;
+	}
+
+	if(token.type == TOKEN_COMMENT)
+	{
+		printf("Encountered comment, reading to end of line\n");
+		free(token.string);
+
+		//Consume until end of comment
+		char car = ' ';
+		while(car != '\n')
+		{
+			car = getc(source_file);
+		}
+
+		return true;
+	}
+
+	if(token.type == TOKEN_SEMICOLON)
+	{
+		printf("Reached end of statement\n");
+		free(token.string);
+		return true;
+	}
+
+	if(token.type == TOKEN_WORD)
+	{
+		printf("Word found: %s\n", token.string);
+		if(strcmp(token.string, "def") == 0)
+		{
+			//We must be dealing with a variable definition
+			struct NODE *new_node = create_node(AST_DEF);
+			new_node->stack_location = current_parse_parent_node->stack_len;
+			current_parse_parent_node->stack_len += 1;
+			add_node(new_node);
+		}
+	}
+	else if(token.type == TOKEN_COLON)
+		printf("Colon found\n");
+
+	free(token.string);
+	return true;
+}
---
+++ compiler/parser.h
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+
+enum TOKEN_TYPE {TOKEN_WORD, TOKEN_COLON, TOKEN_SEMICOLON, TOKEN_COMMENT};
+
+struct TOKEN
+{
+	bool valid;
+	enum TOKEN_TYPE type;
+	char *string;
+};
+
+
+bool parse_next_statement(FILE *source_file);
