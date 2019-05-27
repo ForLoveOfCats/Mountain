d5dbfa49e32dd00dbd45807f5e63d5509ee8a277
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-05-27 14:54:49 -0400

    Cat resulting C source in debug script

--- Debug.sh
+++ Debug.sh
@@ -7,6 +7,9 @@ if [ $? -eq 0 ]; then #if the compiler was built correctly
 	./build/compiler ./output/output.c ./examples/example.mountain #run the compiler
 
 	if [ $? -eq 0 ]; then #if the compiler succeeded
+		printf "\n\n===== Resulting C Source =====\n"
+		cat ./output/output.c
+
 		printf "\n\n===== Transpiled Source Building =====\n"
 		gcc -fsanitize=address -g -o ./output/output ./output/output.c #compile the transipile results
 
