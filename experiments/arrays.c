87abac514a165536289f2a1c6b1779bed835d298
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-08-14 18:01:25 -0400

    Update arrays experiment

--- experiments/arrays.c
+++ experiments/arrays.c
@@ -1,6 +1,57 @@
-// This file intends to experiment with how the Mountain array type should function internally
-// It can be compiled with:
-// gcc -g -fsanitize=address -Wall -Wextra -Wpedantic arrays.c -o arrays
+//This file intends to experiment with how the Mountain T:Array type will function internally
+//The T:Array type assumes that the size will always be known at compile time
+//When the size needs to be caculated at compile time one would instead use T:HeapArray
+//This T:HeapArray type amounts to little more than a size and a pointer to a malloced array
+
+//Compile with:
+// gcc -g -fsanitize=address -fsanitize=undefined -Wall -Wextra -Wpedantic arrays.c -o arrays
+//or
+// clang -g -fsanitize=address -fsanitize=undefined -Wall -Wextra -Wpedantic arrays.c -o arrays
+
+
+
+//The below code is roughly equivalent to the following Mountain code
+//(If you spot any differences between it and the below C code please open a bug)
+#if 0 //Change to a 1 to get syntax highlighting in your editor
+module main;
+
+
+func:Void main() {
+	{
+		let array = new:i32:Array(5)::();
+
+		for(let index = 0; x <= 4, x += 1) {
+			array.at(x) |= x; // T:Array.at(i32) returns a T:Ptr
+		}
+
+		println(array.at(x)|to_string());
+	}
+
+	{
+		let array = [0, 1, 2, 3, 4];
+		println(array.at(x)|to_string());
+	}
+
+	{
+		let array = new:i32:Array(5)::heap();
+
+		for(let index = 0; x <= 4, x += 1) {
+			array|at(x) |= x;
+		}
+
+		println(array|at(x)|to_string());
+		array.free();
+	}
+
+	{
+		let array = new:i32:Array(5)::heap([0, 1, 2, 3, 4]);
+		//Sidenote: This constructor would not have the cost of passing the stack array as all constructors
+		// are macros in Mountain so this would deconstruct to something very efficient
+		println(array|at(x)|to_string());
+		array.free();
+	}
+}
+#endif
 
 
 
@@ -10,73 +61,79 @@
 
 
 
-struct ArrayIntT
+//The Mountain compiler would generate a version of this for every type and length combination used
+struct IntArray5
 {
-	int *head;
-	int length;
+	int head[5];
 };
 
 
-struct ArrayIntT StackArrayInt(int *head, int length) //The space on the stack is at callsite and is provided by the language semantics
-{
-	struct ArrayIntT instance = {
-		head,
-		length
-	};
+//The use of macros here is simply to "simulate" the Mountain Array semantics
+//Some of them could use __VA_ARGS__ but do not in order to clarify that this would all be handled by code generation
 
-	return instance;
-}
+#define STACK_INT_ARRAY_5_EMPTY(name) struct IntArray5 name
+#define STACK_INT_ARRAY_5_DEFAULT_VALUES(name, item0, item1, item2, item3, item4) struct IntArray5 name = { {item0, item1, item2, item3, item4} }
 
-//This macro emulates the Mountain stack array semantics
-#define STACK_ARRAY_INT(NAME, LENGTH) \
-	int NAME##_ARRAY_INT_STACK_STORE[LENGTH]; \
-	struct ArrayIntT NAME = StackArrayInt(NAME##_ARRAY_INT_STACK_STORE, LENGTH); \
+#define HEAP_INT_ARRAY_5_EMPTY(name) struct IntArray5 *name = (struct IntArray5*)malloc(sizeof(struct IntArray5))
+#define HEAP_INT_ARRAY_5_DEFAULT_VALUES(name, item0, item1, item2, item3, item4) struct IntArray5 *name = (struct IntArray5*)malloc(sizeof(struct IntArray5)); \
+	{ \
+		name->head[0] = item0; \
+		name->head[1] = item1; \
+		name->head[2] = item2; \
+		name->head[3] = item3; \
+		name->head[4] = item4; \
+	} \
 
 
-struct ArrayIntT *HeapArrayInt(int length) //The array itself and ArrayIntT are both on the heap
-{
-	struct ArrayIntT *instance = malloc(sizeof(struct ArrayIntT));
-	instance->head = malloc(sizeof(int) * length);
-	instance->length = length;
-
-	return instance;
-}
-
-void HeapArrayIntDeconstructor(struct ArrayIntT *instance)
-{
-	free(instance->head);
-	free(instance);
-}
+#define STACK_INT_ARRAY_AT(array, index) array.head[index]
+#define HEAP_INT_ARRAY_AT(array, index) array->head[index]
 
 
 int main()
 {
-	STACK_ARRAY_INT(stack_array_var, 5); /* In Mountain this would be as follows?
-	let:i32:Array stack_array_var = i32:Array::(5);
-		or
-	let stack_array_var = i32:Array::(5); */
-	printf("Stack allocated array of ints\n");
-	for(int index = 0; index <= 4; index++)
 	{
-		assert(index <= stack_array_var.length-1);
-		stack_array_var.head[index] = index; //The head[index] will be hidden by the .At and .Set of the Mountain array
-		printf("%i\n", stack_array_var.head[index]);
+		//Stack allocated array of 5 ints filled with garbage data
+		STACK_INT_ARRAY_5_EMPTY(array); //let array = new:i32:Array(5)::();
+		for(int x = 0; x <= 4; x++)
+			STACK_INT_ARRAY_AT(array, x) = x; //array.at(x) |= x;
+		for(int x = 0; x <= 4; x++) //println(array.at(x)|to_string());
+			printf("%i\n", STACK_INT_ARRAY_AT(array, x));
 	}
 
 
 	printf("\n");
 
 
-	printf("Heap allocated array of ints\n");
-	struct ArrayIntT *heap_array_var = HeapArrayInt(5);/* In Mountain this would be as follows
-	let:i32:Array:Ptr heap_array_var = i32:Array::heap(5);
-		or
-	let heap_array_var = i32:Array::heap(5); */
-	for(int index = 0; index <= 4; index++)
 	{
-		assert(index <= heap_array_var->length-1);
-		heap_array_var->head[index] = index;
-		printf("%i\n", stack_array_var.head[index]);
+		//Stack allocated array of 5 ints filled with default data
+		STACK_INT_ARRAY_5_DEFAULT_VALUES(array, 0, 1, 2, 3, 4); //let array = [0, 1, 2, 3, 4];
+		for(int x = 0; x <= 4; x++) //println(array.at(x)|to_string());
+			printf("%i\n", STACK_INT_ARRAY_AT(array, x));
 	}
-	HeapArrayIntDeconstructor(heap_array_var);
+
+
+	printf("\n");
+
+
+	{
+		//Heap allocated array of 5 ints filled with garbage data
+		HEAP_INT_ARRAY_5_EMPTY(array); //let array = new:i32:Array(5)::heap();
+		for(int x = 0; x <= 4; x++)
+			HEAP_INT_ARRAY_AT(array, x) = x; //array|at(x) |= x;
+		for(int x = 0; x <= 4; x++) //println(array|at(x)|to_string());
+			printf("%i\n", HEAP_INT_ARRAY_AT(array, x));
+		free(array); //array.free();
+	}
+
+
+	printf("\n");
+
+
+	{
+		//Heap allocated array of 5 ints filled with default data
+		HEAP_INT_ARRAY_5_DEFAULT_VALUES(array, 0, 1, 2, 3, 4); //let array = new:i32:Array(5)::heap([0, 1, 2, 3, 4]);
+		for(int x = 0; x <= 4; x++) //println(array|at(x)|to_string());
+			printf("%i\n", HEAP_INT_ARRAY_AT(array, x));
+		free(array); //array.free();
+	}
 }
