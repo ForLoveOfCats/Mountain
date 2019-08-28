9869d006111a911b071d8e0e652a4fad12c15f1d
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-08-28 12:55:36 -0400

    Compile emitted C with Clang and undefined behavior sanitizer

--- Debug.sh
+++ Debug.sh
@@ -11,7 +11,7 @@ if [ $? -eq 0 ]; then #if the compiler was built correctly
 		cat ./output/output.c
 
 		printf "\n\n===== Transpiled Source Building =====\n"
-		gcc -g -fsanitize=address -Wreturn-type -Wpedantic -o ./output/output ./output/output.c #compile the transpile results
+		clang -g -fsanitize=address -fsanitize=undefined -Wreturn-type -Wpedantic -o ./output/output ./output/output.c #compile the transpile results
 
 			if [ $? -eq 0 ]; then #if the transpiled source compiled correctly
 				printf "\n\n===== Executing Final Binary =====\n"
