0f23e86b673b414d1af762cf5aed1891829b7f7d
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-09-05 15:56:48 -0400

    Add basic test suite

---
+++ Test.sh
@@ -0,0 +1,21 @@
+
+printf "===== Compiler Building =====\n"
+scons #build
+
+if [ $? -eq 0 ]; then #if the compiler was built correctly
+	printf "\n\n===== Tests Building =====\n"
+	./build/compiler test ./tests ./output/output.c #run the compiler
+
+	if [ $? -eq 0 ]; then #if the compiler succeeded
+		printf "\n\n===== Resulting C Source =====\n"
+		cat ./output/output.c
+
+		printf "\n\n===== Transpiled Source Building =====\n"
+		clang -g -fsanitize=address -fsanitize=undefined -Wreturn-type -Wpedantic -o ./output/output ./output/output.c #compile the transpile results
+
+			if [ $? -eq 0 ]; then #if the transpiled source compiled correctly
+				printf "\n\n===== Executing Final Binary =====\n"
+				./output/output #run the final binary
+			fi
+	fi
+fi
