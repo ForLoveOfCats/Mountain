2658efddfa9d276d07a5f51c32b7425b14d0a154
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-11-26 20:03:33 -0500

    Set token kind

--- compiler/parser/parse_file.zig
+++ compiler/parser/parse_file.zig
@@ -5,5 +5,8 @@ usingnamespace tokenizer;
 
 
 pub fn parse_file(tokens: []Token) anyerror!void {
+    for(tokens) |token| {
+        println("'{}' of {}", token.string, token.kind);
+    }
 }
 
--- compiler/tokenizer.zig
+++ compiler/tokenizer.zig
@@ -2,12 +2,45 @@ usingnamespace @import("imports.zig");
 
 
 
+pub const tKind = enum {
+    Word,
+    Colon,
+    Period,
+    Semicolon,
+    Equals,
+    CompEqual,
+    CompNotEqual,
+    CompGreater,
+    CompGreaterEqual,
+    CompLess,
+    CompLessEqual,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Exclamation,
+    AddressOf,
+    Dereference,
+    OpenParentheses,
+    CloseParentheses,
+    OpenBrace,
+    CloseBrace,
+    OpenBracket,
+    CloseBracket,
+    Comma,
+    Hash,
+};
+
+
 pub const Token = struct {
     line: LineNumber,
     column_start: CharNumber,
     column_end: CharNumber,
+
     start: usize,
     end: usize,
+
+    kind: tKind,
     string: []u8,
 };
 
@@ -54,6 +87,7 @@ pub fn tokenize_file(source: []const u8, tokens: *std.ArrayList(Token)) !void {
         .column_end = newCharNumber(0),
         .start = 0,
         .end = 0,
+        .kind = .Word,
         .string = [_]u8 {},
     };
     while(iterator.nextCodepoint()) |codepoint32| {
@@ -75,6 +109,7 @@ pub fn tokenize_file(source: []const u8, tokens: *std.ArrayList(Token)) !void {
                 .column_end = newCharNumber(column),
                 .start = point.start,
                 .end = point.end,
+                .kind = .Word,
                 .string = [_]u8 {},
             };
         }
@@ -84,12 +119,13 @@ pub fn tokenize_file(source: []const u8, tokens: *std.ArrayList(Token)) !void {
             ' ', '\t', '\n' => {
                 if(token.string.len > 0) {
                     try tokens.append(token);
-                    token = Token  {
+                    token = Token {
                         .line = newLineNumber(line),
                         .column_start = newCharNumber(column),
                         .column_end = newCharNumber(column),
                         .start = point.start,
                         .end = point.end,
+                        .kind = .Word,
                         .string = [_]u8 {},
                     };
                 }
@@ -123,20 +159,97 @@ pub fn tokenize_file(source: []const u8, tokens: *std.ArrayList(Token)) !void {
                         .column_end = newCharNumber(column),
                         .start = point.start,
                         .end = point.end,
+                        .kind = .Word,
                         .string = [_]u8 {},
                     };
                     column -= 1;
                     iterator.i -= point.len;
                 }
                 else {
-                    try tokens.append(Token {
+                    var char_token = Token {
                         .line = newLineNumber(line),
                         .column_start = newCharNumber(column),
                         .column_end = newCharNumber(column+1),
                         .start = point.start,
                         .end = point.end,
+                        .kind = switch(point.bytes[0]) {
+                            ':' => .Colon,
+                            '.' => .Period,
+                            ';' => .Semicolon,
+                            ',' => .Comma,
+                            '#' => .Hash,
+                            '=' => .Equals,
+                            '>' => .CompGreater,
+                            '<' => .CompLess,
+                            '+' => .Plus,
+                            '-' => .Minus,
+                            '*' => .Multiply,
+                            '/' => .Divide,
+                            '!' => .Exclamation,
+                            '&' => .AddressOf,
+                            '$' => .Dereference,
+                            '(' => .OpenParentheses,
+                            ')' => .CloseParentheses,
+                            '{' => .OpenBrace,
+                            '}' => .CloseBrace,
+                            '[' => .OpenBracket,
+                            ']' => .CloseBracket,
+
+                            else => unreachable,
+                        },
                         .string = source[point.start .. point.end+1],
-                    });
+                    };
+
+                    if(char_token.kind == .Equals) fail: {
+                        var peeked = try StreamCodePoint8.init(iterator.nextCodepoint() orelse break :fail, iterator.i-1);
+                        if(peeked.bytes[0] == '=') {
+                            char_token.kind = .CompEqual;
+                            char_token.column_end.number += 1; //We know we can't have gone to the next line
+                            char_token.end += 1;
+                            char_token.string = source[char_token.start .. peeked.end+1];
+                        }
+                        else {
+                            iterator.i -= peeked.len;
+                        }
+                    }
+                    else if(char_token.kind == .CompGreater) fail: {
+                        var peeked = try StreamCodePoint8.init(iterator.nextCodepoint() orelse break :fail, iterator.i-1);
+                        if(peeked.bytes[0] == '=') {
+                            char_token.kind = .CompGreaterEqual;
+                            char_token.column_end.number += 1; //We know we can't have gone to the next line
+                            char_token.end += 1;
+                            char_token.string = source[char_token.start .. peeked.end+1];
+                        }
+                        else {
+                            iterator.i -= peeked.len;
+                        }
+                    }
+                    else if(char_token.kind == .CompLess) fail: {
+                        var peeked = try StreamCodePoint8.init(iterator.nextCodepoint() orelse break :fail, iterator.i-1);
+                        if(peeked.bytes[0] == '=') {
+                            char_token.kind = .CompLessEqual;
+                            char_token.column_end.number += 1; //We know we can't have gone to the next line
+                            char_token.end += 1;
+                            char_token.string = source[char_token.start .. peeked.end+1];
+                        }
+                        else {
+                            iterator.i -= peeked.len;
+                        }
+                    }
+                    else if(char_token.kind == .Exclamation) fail: {
+                        var peeked = try StreamCodePoint8.init(iterator.nextCodepoint() orelse break :fail, iterator.i-1);
+                        if(peeked.bytes[0] == '=') {
+                            char_token.kind = .CompNotEqual;
+                            char_token.column_end.number += 1; //We know we can't have gone to the next line
+                            char_token.end += 1;
+                            char_token.string = source[char_token.start .. peeked.end+1];
+                        }
+                        else {
+                            iterator.i -= peeked.len;
+                        }
+                    }
+
+                    try tokens.append(char_token);
                 }
 
                 continue;
