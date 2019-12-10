fffc8c6a039828c69b531027d78f4e7cb57f3607
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-12-10 13:29:13 -0500

    Parse all contents of a block

--- compiler/parser/parse_block.zig
+++ compiler/parser/parse_block.zig
@@ -11,16 +11,28 @@ pub fn parse_block(self: *TokenIterator, block: *pBlock, global: bool) anyerror!
         self.next();
     }
 
-    if(mem.eql(u8, self.token().string, "func")) {
-        var func = try parse_func(self);
-        try block.contents.append(pBlock.InBlock {.Func = func});
-    }
+    while(true) {
+        if(mem.eql(u8, self.token().string, "func")) {
+            var func = try parse_func(self);
+            try block.contents.append(pBlock.InBlock {.Func = func});
+        }
 
-    else {
-        var expression = try parse_expression(self);
-        try block.contents.append(pBlock.InBlock {.Expression = expression});
+        else {
+            var expression = try parse_expression(self);
+            try block.contents.append(pBlock.InBlock {.Expression = expression});
 
+            self.next();
+            expect_kind(self.token(), .Semicolon);
+        }
+
+        if(global and !self.has_next()) {
+            return;
+        }
+        else if(!global and self.peek().kind == .CloseBrace) {
+            self.next();
+            expect_kind(self.token(), .CloseBrace); //Sanity check
+            return;
+        }
         self.next();
-        expect_kind(self.token(), .Semicolon);
     }
 }
