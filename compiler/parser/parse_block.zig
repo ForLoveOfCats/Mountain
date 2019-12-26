45f5465b6a035f40cd1ebf64236ce478dde779c6
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-12-26 18:25:48 -0500

    Properly parse blank blocks

--- compiler/parser/parse_block.zig
+++ compiler/parser/parse_block.zig
@@ -11,11 +11,20 @@ pub fn parse_block(self: *TokenIterator, block: *pBlock, global: bool) anyerror!
     }
 
     while(true) {
+        //Short circut if reached the end of the block
+        if(global and !self.has_next()) {
+            return;
+        }
+        else if(!global and self.token().kind == .CloseBrace) {
+            expect_kind(self.token(), .CloseBrace); //Sanity check
+            return;
+        }
+
+        //Parse the next item in block
         if(mem.eql(u8, self.token().string, "func")) {
             var func = try parse_func(self);
             try block.contents.append(pBlock.InBlock {.Func = func});
         }
-
         else {
             var expression = try parse_expression(self);
             try block.contents.append(pBlock.InBlock {.Expression = expression});
@@ -24,14 +33,7 @@ pub fn parse_block(self: *TokenIterator, block: *pBlock, global: bool) anyerror!
             expect_kind(self.token(), .Semicolon);
         }
 
-        if(global and !self.has_next()) {
-            return;
-        }
-        else if(!global and self.peek().kind == .CloseBrace) {
+        if(self.has_next())
             self.next();
-            expect_kind(self.token(), .CloseBrace); //Sanity check
-            return;
-        }
-        self.next();
     }
 }
