c5bd97ce5fdb4ef17f5f85f552126c1663529b67
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-12-27 22:55:05 -0500

    Add math to expression union (not parsed yet)

--- compiler/parser/ptypes.zig
+++ compiler/parser/ptypes.zig
@@ -119,8 +119,52 @@ pub const pBlock = struct {
 };
 
 
+pub const pMathKind = enum {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+};
+
+
+pub const pMath = struct {
+    kind: pMathKind,
+    left: *pExpression,
+    right: *pExpression,
+
+    pub fn deinit(self: *pMath) void {
+        self.left.deinit();
+        self.right.deinit();
+    }
+
+    pub fn debug_print(self: pMath, level: usize) void {
+        debug_print_level(level);
+        println("{}:", @tagName(self.kind));
+        self.left.debug_print(level+1);
+        self.right.debug_print(level+1);
+    }
+};
+
+
+pub const pNegate = struct {
+    expression: *pExpression,
+
+    pub fn deinit(self: *pNegate) void {
+        self.expression.deinit();
+    }
+
+    pub fn debug_print(self: pNegate, level: usize) void {
+        debug_print_level(level);
+        println("Negate:");
+        self.expression.debug_print(level+1);
+    }
+};
+
+
 pub const pExpression = union(enum) {
     Let: pLet,
+    Negate: pNegate,
+    Math: pMath,
 
     //This feels like a hack but allows the allocation to be done in
     //the init which is consistant and less surprising in the long
@@ -134,12 +178,19 @@ pub const pExpression = union(enum) {
 
     pub fn deinit(self: *pExpression) void {
         switch(self.*) {
-            .Let => |*let| {
-                let.deinit();
-            },
+            .Let => |*let| let.deinit(),
+            .Negate => |*negate| negate.deinit(),
+            .Math => |*math| math.deinit(),
         }
 
         heap.c_allocator.destroy(self);
+    }
+
+    pub fn is_math(self: pExpression) bool {
+        return switch(self) {
+            Math => true,
+            else => false,
+        };
     }
 
     pub fn debug_print(self: pExpression, level: usize) void {
@@ -148,6 +199,8 @@ pub const pExpression = union(enum) {
 
         switch(self) {
             .Let => |let| let.debug_print(level+1),
+            .Negate => |negate| negate.debug_print(level+1),
+            .Math => |math| math.debug_print(level+1),
         }
     }
 };
