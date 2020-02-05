a36ddbe4a90e29b6e784278c4a4ec843ec5781a2
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2020-02-04 20:17:00 -0500

    Add run step to build.zig

--- build.zig
+++ build.zig
@@ -12,5 +12,12 @@ pub fn build(b: *Builder) void
 
     exe.linkSystemLibrary("c");
 
+    const run_cmd = exe.run();
+    if(b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
     exe.install();
 }
