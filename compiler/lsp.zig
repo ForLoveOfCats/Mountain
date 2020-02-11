06d93ba7c5ae2ac2c91a88ba033f892ecb238952
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2020-02-11 16:56:55 -0500

    Start understanding json rpc

--- compiler/lsp.zig
+++ compiler/lsp.zig
@@ -2,7 +2,32 @@ usingnamespace @import("imports.zig");
 
 
 
-fn read_rpc(instream: *fs.File.InStream.Stream) ![]u8 {
+const Id = union(enum) {
+    String: []const u8,
+    Integer: i64,
+};
+
+
+const Request = struct {
+    id: Id,
+    method: []const u8,
+    params: *json.ObjectMap,
+};
+
+
+const Notification = struct {
+    method: []const u8,
+    params: *json.ObjectMap,
+};
+
+
+const Rpc = union(enum) {
+    Request: Request,
+    Notification: Notification,
+};
+
+
+fn read_json(instream: *fs.File.InStream.Stream, arena: *mem.Allocator) ![]u8 {
     var buffer: [1024 * 4]u8 = undefined;
     var content_length: ?usize = null;
 
@@ -20,7 +45,7 @@ fn read_rpc(instream: *fs.File.InStream.Stream) ![]u8 {
         }
     }
 
-    var content = try allocator.alloc(u8, content_length.?);
+    var content = try arena.alloc(u8, content_length.?);
     const read_length = try instream.read(content);
 
     if(content_length.? != read_length) {
@@ -31,14 +56,74 @@ fn read_rpc(instream: *fs.File.InStream.Stream) ![]u8 {
 }
 
 
+fn read_rpc(instream: *fs.File.InStream.Stream, arena: *mem.Allocator) !Rpc {
+    var content = try read_json(instream, arena);
+
+    var rpc_parser = json.Parser.init(arena, false);
+    defer rpc_parser.deinit();
+
+    var tree = try rpc_parser.parse(content);
+    var object = tree.root.Object;
+
+    if(object.get("id")) |id| { //Request
+        var actual_id: ?Id = undefined;
+        switch(id.value) {
+            .String => |str| {
+                actual_id = Id {
+                    .String = str,
+                };
+            },
+
+            .Integer => |int| {
+                actual_id = Id {
+                    .Integer = int,
+                };
+            },
+
+            else => unreachable,
+        }
+
+        return Rpc {
+            .Request = Request {
+                .id = actual_id.?,
+                .method = object.get("method").?.value.String,
+                .params = &object.get("params").?.value.Object,
+            },
+        };
+    }
+    else { //Notification
+        return Rpc {
+            .Notification = Notification {
+                .method = object.get("method").?.value.String,
+                .params = &object.get("params").?.value.Object,
+            },
+        };
+    }
+}
+
+
 pub fn serve() !void {
     var instream = &io.getStdIn().inStream().stream;
 
     while(true) {
-        var content = try read_rpc(instream);
-        defer allocator.free(content);
+        var arena = heap.ArenaAllocator.init(allocator);
+        defer arena.deinit();
 
-        println("{}", .{content});
-        println("", .{});
+        var rpc = try read_rpc(instream, &arena.allocator);
+
+        switch(rpc) {
+            .Request => |request| {
+                print("Recieved request '{}' of id ", .{request.method});
+
+                switch(request.id) {
+                    .String => |id| println("'{}'", .{id}),
+                    .Integer => |id| println("'{}'", .{id}),
+                }
+            },
+
+            .Notification => |notification| {
+                print("Recieved notification '{}'", .{notification.method});
+            },
+        }
     }
 }
